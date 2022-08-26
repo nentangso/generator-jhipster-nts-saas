37381f3156f744e894d7d32c076fbac2d04d94b4
tientq <tientq@outlook.com>
2022-08-26 09:24:02 +0700

    enable hooks

--- generators/server/generator.mjs
+++ generators/server/generator.mjs
@@ -1,6 +1,18 @@
 import chalk from 'chalk';
 import ServerGenerator from 'generator-jhipster/esm/generators/server';
-import { PRIORITY_PREFIX, WRITING_PRIORITY, POST_WRITING_PRIORITY } from 'generator-jhipster/esm/priorities';
+import {
+  PRIORITY_PREFIX,
+  INITIALIZING_PRIORITY,
+  PROMPTING_PRIORITY,
+  CONFIGURING_PRIORITY,
+  COMPOSING_PRIORITY,
+  LOADING_PRIORITY,
+  PREPARING_PRIORITY,
+  DEFAULT_PRIORITY,
+  WRITING_PRIORITY,
+  POST_WRITING_PRIORITY,
+  END_PRIORITY,
+} from 'generator-jhipster/esm/priorities';
 
 export default class extends ServerGenerator {
   constructor(args, opts, features) {
@@ -13,6 +25,48 @@ export default class extends ServerGenerator {
     }
 
     this.sbsBlueprint = true;
+  }
+
+  get [INITIALIZING_PRIORITY]() {
+    return {
+      async initializingTemplateTask() {},
+    };
+  }
+
+  get [PROMPTING_PRIORITY]() {
+    return {
+      async promptingTemplateTask() {},
+    };
+  }
+
+  get [CONFIGURING_PRIORITY]() {
+    return {
+      async configuringTemplateTask() {},
+    };
+  }
+
+  get [COMPOSING_PRIORITY]() {
+    return {
+      async composingTemplateTask() {},
+    };
+  }
+
+  get [LOADING_PRIORITY]() {
+    return {
+      async loadingTemplateTask() {},
+    };
+  }
+
+  get [PREPARING_PRIORITY]() {
+    return {
+      async preparingTemplateTask() {},
+    };
+  }
+
+  get [DEFAULT_PRIORITY]() {
+    return {
+      async defaultTemplateTask() {},
+    };
   }
 
   get [WRITING_PRIORITY]() {
@@ -31,6 +85,12 @@ export default class extends ServerGenerator {
   get [POST_WRITING_PRIORITY]() {
     return {
       async postWritingTemplateTask() {},
+    };
+  }
+
+  get [END_PRIORITY]() {
+    return {
+      async endTemplateTask() {},
     };
   }
 }
