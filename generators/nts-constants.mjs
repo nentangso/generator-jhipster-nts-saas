c9cb49bf5968cc8869d233e63de0bd1f3873c0bb
tientq <tientq@outlook.com>
2023-04-17 23:48:25 +0700

    Update nts-saas@1.1.9

--- generators/nts-constants.mjs
+++ generators/nts-constants.mjs
@@ -2,5 +2,5 @@ export const MEDIA_PROVIDERS = {
   S3: 'S3',
 };
 
-export const NTS_SAAS_DEPENDENCIES_VERSION = '1.1.7';
+export const NTS_SAAS_DEPENDENCIES_VERSION = '1.1.9';
 export const NTS_SAAS_REACT_VERSION = '^1.0.0';
