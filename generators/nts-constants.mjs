fb90ff85bd36d778b02326c9ee893230d8a82779
tientq <tientq@outlook.com>
2023-07-05 23:59:28 +0700

    Update nts-saas to 1.3.0-RC7

--- generators/nts-constants.mjs
+++ generators/nts-constants.mjs
@@ -2,5 +2,5 @@ export const MEDIA_PROVIDERS = {
   S3: 'S3',
 };
 
-export const NTS_SAAS_DEPENDENCIES_VERSION = '1.3.0-RC6';
+export const NTS_SAAS_DEPENDENCIES_VERSION = '1.3.0-RC7';
 export const NTS_SAAS_REACT_VERSION = '^1.0.0';
